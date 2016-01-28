f8fe74d642a2459b2ae307e3a864a72dc01b6f83
Himanshu Gupta <g.himanshu@gmail.com>
2016-01-28 16:18:22 -0600

    use correct ObjectMapper in Index[IO/Merger] in AggregationTestHelper

--- processing/src/test/java/io/druid/query/aggregation/AggregationTestHelper.java
+++ processing/src/test/java/io/druid/query/aggregation/AggregationTestHelper.java
@@ -69,9 +69,8 @@ import io.druid.segment.IndexSpec;
 import io.druid.segment.QueryableIndex;
 import io.druid.segment.QueryableIndexSegment;
 import io.druid.segment.Segment;
-import io.druid.segment.TestHelper;
+import io.druid.segment.column.ColumnConfig;
 import io.druid.segment.incremental.IncrementalIndex;
-import io.druid.segment.incremental.IndexSizeExceededException;
 import io.druid.segment.incremental.OnheapIncrementalIndex;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
@@ -165,10 +164,22 @@ public class AggregationTestHelper
         pool
     );
 
+    IndexIO indexIO = new IndexIO(
+        mapper,
+        new ColumnConfig()
+        {
+          @Override
+          public int columnCacheSizeBytes()
+          {
+            return 0;
+          }
+        }
+    );
+
     return new AggregationTestHelper(
         mapper,
-        TestHelper.getTestIndexMerger(),
-        TestHelper.getTestIndexIO(),
+        new IndexMerger(mapper, indexIO),
+        indexIO,
         toolchest,
         factory,
         tempFolder,
@@ -197,10 +208,22 @@ public class AggregationTestHelper
         QueryRunnerTestHelper.NOOP_QUERYWATCHER
     );
 
+    IndexIO indexIO = new IndexIO(
+        mapper,
+        new ColumnConfig()
+        {
+          @Override
+          public int columnCacheSizeBytes()
+          {
+            return 0;
+          }
+        }
+    );
+
     return new AggregationTestHelper(
         mapper,
-        TestHelper.getTestIndexMerger(),
-        TestHelper.getTestIndexIO(),
+        new IndexMerger(mapper, indexIO),
+        indexIO,
         toolchest,
         factory,
         tempFolder,
