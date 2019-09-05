e03a520f5a2d2555ffc63fbede99d934e56b8b15
Himanshu Gupta <g.himanshu@gmail.com>
2019-09-05 14:21:14 -0700

    UTs for ExtractionFnVirtualColumn WIP

---
+++ processing/src/test/java/org/apache/druid/segment/virtual/ExtractionFnVirtualColumnTest.java
@@ -0,0 +1,14 @@
+package org.apache.druid.segment.virtual;
+
+import org.junit.Test;
+
+public class ExtractionFnVirtualColumnTest
+{
+  // it might be also good idea to test it on deployed cluster to see if it really works in groupBy/timeseries/topN
+  // queries as you think it should
+  @Test
+  public void testMakeDimensionSelector()
+  {
+
+  }
+}
